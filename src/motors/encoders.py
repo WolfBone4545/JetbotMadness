b0dfbf60501284b662cdd3700052c97649a2a1a8
LosVocelos <janocelik@gmail.com>
2024-08-16 05:17:08 +0200

    Idk, it still no workie... I mean, it kinda does, but still...

--- src/motors/encoders.py
+++ src/motors/encoders.py
@@ -32,13 +32,14 @@ class decoder:
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup([gpioA, gpioB], GPIO.IN)
-        self.cbAr = GPIO.add_event_detect(self.gpioA, GPIO.RISING, callback=self._cb_A_r, bouncetime=None, polltime=1)
-        self.cbAf = GPIO.add_event_detect(self.gpioA, GPIO.FALLING, callback=self._cb_A_f, bouncetime=None, polltime=1)
-        self.cbBr = GPIO.add_event_detect(self.gpioB, GPIO.RISING, callback=self._cb_B_r, bouncetime=None, polltime=1)
-        self.cbBf = GPIO.add_event_detect(self.gpioB, GPIO.FALLING, callback=self._cb_B_f, bouncetime=None, polltime=1)
+        self.cbAr = GPIO.add_event_detect(self.gpioA, GPIO.RISING, callback=self._cb_A_r, bouncetime=None)
+        # self.cbAf = GPIO.add_event_detect(self.gpioA, GPIO.FALLING, callback=self._cb_A_f, bouncetime=None)
+        self.cbBr = GPIO.add_event_detect(self.gpioB, GPIO.RISING, callback=self._cb_B_r, bouncetime=None)
+        # self.cbBf = GPIO.add_event_detect(self.gpioB, GPIO.FALLING, callback=self._cb_B_f, bouncetime=None)
 
     def _cb_A_r(self, gpio):
         self.levA = 1
+        self.levB = GPIO.input(self.gpioB)
         if self.levB == 1:
             self.callback(1)
 
@@ -47,6 +48,7 @@ class decoder:
 
     def _cb_B_r(self, gpio):
         self.levB = 1
+        self.levA = GPIO.input(self.gpioA)
         if self.levA == 1:
             self.callback(-1)
 
@@ -63,12 +65,10 @@ if __name__ == "__main__":
     def callback_l(way):
         global pos_l
         pos_l += way
-        print("pos_l={}".format(pos_l))
 
     def callback_r(way):
         global pos_r
         pos_r += way
-        print("pos_r={}".format(pos_r))
 
     decoder_l = decoder(22, 10, callback_l)
     decoder_r = decoder( 9, 11, callback_r)
@@ -77,7 +77,9 @@ if __name__ == "__main__":
 
     try:
         while True:
-            pass
+            print("pos_l={}".format(pos_l))
+            print("pos_r={}".format(pos_r))
+            time.sleep(0.5)
     finally:
         GPIO.cleanup()  # cleanup all GPIOs
         print("endung")
