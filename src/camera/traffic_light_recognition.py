2effb480888c275a6c8f40923e914f832cf4b5ba
LosVocelos <janocelik@gmail.com>
2024-08-22 17:50:38 +0200

    fixed thresholding

--- src/camera/traffic_light_recognition.py
+++ src/camera/traffic_light_recognition.py
@@ -10,14 +10,17 @@ def traffic_light_recognition(image):
 def detect_traffic_light(img):
     blue, green, red = cv2.split(img)
 
-    blur_g = cv2.GaussianBlur(green, (5, 5), 0)
-    ret, thresh_g = cv2.threshold(blur_g, 0, 255, cv2.THRESH_OTSU)
+    #blur_g = cv2.GaussianBlur(green, (5, 5), 0)
+    #ret, thresh_g = cv2.threshold(blur_g, 200, 255, cv2.THRESH_BINARY)
 
-    blur_r = cv2.GaussianBlur(red, (5, 5), 0)
-    ret, thresh_r = cv2.threshold(blur_r, 0, 255, cv2.THRESH_OTSU)
+    #blur_r = cv2.GaussianBlur(red, (5, 5), 0)
+    #ret, thresh_r = cv2.threshold(blur_r, 200, 255, cv2.THRESH_BINARY)
 
-    cv2.imshow("thresh red", thresh_r)
-    cv2.imshow("thresh green", thresh_g)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    ret, thresh = cv2.threshold(blur, 200, 255, cv2.THRESH_BINARY)
+
+    cv2.imshow("thresh red", thresh)
     cv2.waitKey(1)
 
 
