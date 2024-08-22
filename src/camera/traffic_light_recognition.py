9b33ea1df8e1d1699719266883039b6479a73f10
HelloWorld7894 <danpojhan@protonmail.com>
2024-08-22 17:39:52 +0200

    Traffic light threshold?

--- src/camera/traffic_light_recognition.py
+++ src/camera/traffic_light_recognition.py
@@ -2,17 +2,24 @@ import utils
 import cv2
 import numpy as np
 
-def traffic_light_recognition(image):
-    traffic_img = detect(image)
 
-    cv2.imshow("test", traffic_img)
+def traffic_light_recognition(image):
+    detect_traffic_light(image)
+
+
+def detect_traffic_light(img):
+    blue, green, red = cv2.split(img)
+
+    blur_g = cv2.GaussianBlur(green, (5, 5), 0)
+    ret, thresh_g = cv2.threshold(blur_g, 0, 255, cv2.THRESH_OTSU)
+
+    blur_r = cv2.GaussianBlur(red, (5, 5), 0)
+    ret, thresh_r = cv2.threshold(blur_r, 0, 255, cv2.THRESH_OTSU)
+
+    cv2.imshow("thresh red", thresh_r)
+    cv2.imshow("thresh green", thresh_g)
     cv2.waitKey(1)
 
-
-def detect(img):
-    cimg = img.copy()
-
-    return cimg
 
 
 if __name__ == "__main__":
