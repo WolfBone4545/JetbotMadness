4b7d79489184d6269a3ea9c9a96b5919123c5c2b
Vesek <venda.straka@gmail.com>
2024-08-22 17:41:03 +0200

    Add variable light count

--- traffic_light/src/main.cpp
+++ traffic_light/src/main.cpp
@@ -4,8 +4,10 @@
 //CONFIG
 //--------------------
 
+#define NUM_LIGHTS_AT_ONCE 1
+
 //Sequence of traffic lights
-uint8_t sequence[][2] = {{0, 1}, {2, 3}};
+uint8_t sequence[][NUM_LIGHTS_AT_ONCE] = {{0}, {1}};
 
 //Red, Yellow/Amber, Green
 uint8_t pins[][3]= {{13, 12, 11}, {10, 9, 8}, {5, 6, 7}, {2, 3, 4}};
@@ -26,8 +28,10 @@ static uint8_t step_num = 0;
 void lights_on(uint8_t* step);
 void lights_off(uint8_t* step);
 
+uint8_t i = 0;
+
 void setup() {
-  uint8_t i = 0;
+  i = 0;
   uint8_t j = 0;
 
   for (i=0; i<(sizeof(pins)/sizeof(pins[0])); i++){
@@ -54,25 +58,25 @@ void loop() {
 }
 
 void lights_on(uint8_t* step){
-  digitalWrite(pins[step[0]][1],HIGH); //Yellow/Amber - ON
-  digitalWrite(pins[step[1]][1],HIGH);
+  for (i=0;i<NUM_LIGHTS_AT_ONCE;i++){
+    digitalWrite(pins[step[i]][1],HIGH); //Yellow/Amber - ON
+  }
   delay(YELLOW_LENGTH);
-  digitalWrite(pins[step[0]][0],LOW); //Yellow/Amber, Red - OFF, Green - ON
-  digitalWrite(pins[step[1]][0],LOW);
-  digitalWrite(pins[step[0]][1],LOW);
-  digitalWrite(pins[step[1]][1],LOW);
-  digitalWrite(pins[step[0]][2],HIGH);
-  digitalWrite(pins[step[1]][2],HIGH);
+  for (i=0;i<NUM_LIGHTS_AT_ONCE;i++){
+    digitalWrite(pins[step[i]][0],LOW); //Yellow/Amber, Red - OFF, Green - ON
+    digitalWrite(pins[step[i]][1],LOW);
+    digitalWrite(pins[step[i]][2],HIGH);
+  }
 }
 
 void lights_off(uint8_t* step){
-  digitalWrite(pins[step[0]][2],LOW); //Green - OFF, Yellow/Amber - ON
-  digitalWrite(pins[step[1]][2],LOW);
-  digitalWrite(pins[step[0]][1],HIGH);
-  digitalWrite(pins[step[1]][1],HIGH);
+  for (i=0;i<NUM_LIGHTS_AT_ONCE;i++){
+    digitalWrite(pins[step[i]][1],HIGH);
+    digitalWrite(pins[step[i]][2],LOW); //Green - OFF, Yellow/Amber - ON
+  }
   delay(YELLOW_LENGTH);
-  digitalWrite(pins[step[0]][1],LOW); //Yellow/Amber - OFF, Red - ON
-  digitalWrite(pins[step[1]][1],LOW);
-  digitalWrite(pins[step[0]][0],HIGH);
-  digitalWrite(pins[step[1]][0],HIGH);
+  for (i=0;i<NUM_LIGHTS_AT_ONCE;i++){
+    digitalWrite(pins[step[i]][1],LOW); //Yellow/Amber - OFF, Red - ON
+    digitalWrite(pins[step[i]][0],HIGH);
+  }
 }
