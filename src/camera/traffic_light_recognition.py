f7d10010e8afae5639963da07c993b1834be27e7
LosVocelos <janocelik@gmail.com>
2024-08-22 18:13:48 +0200

    hough line parameters

--- src/camera/traffic_light_recognition.py
+++ src/camera/traffic_light_recognition.py
@@ -9,6 +9,7 @@ def traffic_light_recognition(image):
 
 def detect_traffic_light(img):
     blue, green, red = cv2.split(img)
+    cimg = img.copy()
 
     #blur_g = cv2.GaussianBlur(green, (5, 5), 0)
     #ret, thresh_g = cv2.threshold(blur_g, 200, 255, cv2.THRESH_BINARY)
@@ -20,7 +21,17 @@ def detect_traffic_light(img):
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
     ret, thresh = cv2.threshold(blur, 200, 255, cv2.THRESH_BINARY)
 
+    circles = cv2.HoughCircles(thresh, cv2.HOUGH_GRADIENT, 1, 20, param2=10, minRadius=0, maxRadius=30)
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0,:]:
+            # draw the outer circle
+            cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+            # draw the center of the circle
+            cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+
     cv2.imshow("thresh red", thresh)
+    cv2.imshow("copy img", cimg)
     cv2.waitKey(1)
 
 
