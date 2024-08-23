b38196075450e1140b3024bd5f8cfbdd3df4f024
HelloWorld7894 <danpojhan@protonmail.com>
2024-08-23 18:09:11 +0200

    debug

--- src/camera/traffic_light_recognition.py
+++ src/camera/traffic_light_recognition.py
@@ -28,24 +28,30 @@ def detect_circles(thresh, green, red):
             green_c = green[i[1] - i[2] - 10: i[1] + i[2] + 10, i[0] - i[2] - 10: i[0] + i[2] + 10]
             red_c = red[i[1] - i[2] - 10: i[1] + i[2] + 10, i[0] - i[2] - 10: i[0] + i[2] + 10]
 
-            circles_list.append((i[0], i[1]), i[2])
+            circles_list.append(((i[0], i[1]), i[2], "red"))
 
-            cv2.imshow("test_green", green_c)
-            cv2.imshow("test_red", red_c)
+            # cv2.imshow("test_green", green_c)
+            # cv2.imshow("test_red", red_c)
 
-            cv2.waitKey(0)
+            # cv2.waitKey(0)
 
             # draw the outer circle
             # cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
             # draw the center of the circle
             # cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
 
+    return circles_list
+
 
 def detect_traffic_light(img):
     blue, green, red = cv2.split(img)
     cimg = img.copy()
     thresh = threshold(img)
-    detect_circles(thresh, green, red)
+    circles = detect_circles(thresh, green, red)
+    for circle in circles:
+        cv2.circle(cimg, circle[0], circles[1], (0, 255, 0), 2)
+        cv2.putText(cimg, 'OpenCV', (circle[0][0] - circles[1], circle[0][1] - circles[1]), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1, (0, 255, 0), 2, cv2.LINE_AA)
 
     cv2.imshow("thresh red", thresh)
     cv2.imshow("copy img", cimg)
