b2ccc018d09acc0e701da889483f16f056678366
LosVocelos <janocelik@gmail.com>
2024-08-27 13:36:44 +0200

    Fixed line destringing

--- jetbot-ws/src/lampone/src/main.py
+++ jetbot-ws/src/lampone/src/main.py
@@ -14,10 +14,26 @@ class Main:
         rospy.Subscriber("line_data", String, self.line_callback, queue_size=10)
         rospy.Subscriber("camera", Image, self.camera_callback, queue_size=1)
         rospy.Subscriber("frame", Image, self.frame_callback, queue_size=1)
+        self.muj_image = None
         
 
     def line_callback(self, data):
-        pass
+        points = eval(data.data)
+        
+        rospy.loginfo(data.data)
+        tmp_img = self.muj_image.copy()
+
+        if points is None:
+            return
+
+        for dev in points[0]:
+            cv2.circle(tmp_img, (dev[0], dev[1]), 5, (0, 127, 127), -1)
+
+        for dev in points[1]:
+            cv2.circle(tmp_img, (dev[0], dev[1]), 5, (127, 127, 127), -1)
+
+        cv2.imshow("points", tmp_img)
+        cv2.waitKey(1)
         # rospy.loginfo(data.data)
 
         # Motors are controlled like this:
@@ -29,7 +45,8 @@ class Main:
 
     def camera_callback(self, data):
         image = self.bridge.imgmsg_to_cv2(data, desired_encoding="bgr8")
-        cv2.imshow("camera", image)
+        self.muj_image = image
+        cv2.imshow("camera", self.muj_image)
         cv2.waitKey(1)
 
     def frame_callback(self, data):
@@ -38,7 +55,9 @@ class Main:
         cv2.waitKey(1)
 
     def run(self):
-        rospy.spin()
+        while not rospy.is_shutdown():
+            pass
+
 
 
 if __name__ == "__main__":
